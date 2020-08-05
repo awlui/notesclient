37f273a98102c90c543093c530aeb547464c1e83
awlui <awlui2014@gmail.com>
2020-08-05 01:44:36 -0700

    fix copy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactz
+          Learn React
         </a>
       </header>
     </div>
